fbf523341ba24b98e217bb268e5ed31e6cc8c117
Kimserey <kimserey.lam@groomgy.com>
2018-10-05 09:26:34 +0100

    api

---
+++ Api/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
